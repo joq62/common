bc8ff0b55a2041803b4195f9c6df7100f06ecd5f
joq62 <joq62@c50>
2024-10-12 00:10:38 +0200

    added lib_vm

---
+++ src/lib_vm.erl
@@ -0,0 +1,82 @@
+%%%-------------------------------------------------------------------
+%%% @author c50 <joq62@c50>
+%%% @copyright (C) 2024, c50
+%%% @doc
+%%%
+%%% @end
+%%% Created : 11 Jan 2024 by c50 <joq62@c50>
+%%%-------------------------------------------------------------------
+-module(lib_vm).
+  
+
+ 
+%% API
+
+-export([
+	 check_started/1,
+	 check_stopped/1
+
+	]).
+
+-define(NumTries,500).
+-define(SleepInterval,20).
+
+
+%%%===================================================================
+%%% API
+%%%===================================================================
+
+%%--------------------------------------------------------------------
+%% @doc
+%% 
+%% @end
+%%--------------------------------------------------------------------
+
+check_started(Node)->
+    check_started(Node,?NumTries,?SleepInterval,false).    
+
+ check_started(_Node,_NumTries,_SleepInterval,true)->
+    true;
+ check_started(_Node,0,_SleepInterval,Result)->
+    Result;
+ check_started(Node,NumTries,SleepInterval,false)->
+    case net_adm:ping(Node) of
+	pang->
+	    timer:sleep(SleepInterval),
+	    NewN=NumTries-1,
+	    Result=false;
+	pong->
+	    NewN=NumTries-1,
+	    Result=true
+    end,
+    check_started(Node,NewN,SleepInterval,Result). 
+
+%%--------------------------------------------------------------------
+%% @doc
+%% 
+%% @end
+%%--------------------------------------------------------------------
+check_stopped(Node)->
+    check_stopped(Node,?NumTries,?SleepInterval,false).    
+
+ check_stopped(_Node,_NumTries,_SleepInterval,true)->
+    true;
+ check_stopped(_Node,0,_SleepInterval,Result)->
+    Result;
+ check_stopped(Node,NumTries,SleepInterval,false)->
+    case net_adm:ping(Node) of
+	pong->
+	    timer:sleep(SleepInterval),
+	    NewN=NumTries-1,
+	    Result=false;
+	pang->
+	    NewN=NumTries-1,
+	    Result=true
+    end,
+    check_stopped(Node,NewN,SleepInterval,Result).    
+	    
+
+
+%%%===================================================================
+%%% Internal functions
+%%%===================================================================
